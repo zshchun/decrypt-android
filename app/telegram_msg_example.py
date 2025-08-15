db8767a13c2b676bb58a60fec28274ec83f5c9bb
Seunghwan Chun <zshchun@gmail.com>
2025-08-16 02:55:52 +0900

    add example telegram script

---
+++ app/telegram_msg_example.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sqlite3
+from hexdump import hexdump # simple-hexdump
+conn = sqlite3.connect('cache4.db')
+cursor = conn.cursor()
+
+# Chatroom UID
+chatroom_title = 'the hacker news'
+cursor.execute('''SELECT uid FROM chats WHERE name = ?''', (chatroom_title,))
+uid = cursor.fetchone()[0]
+
+# Show messages
+cursor.execute('''SELECT data FROM messages_v2 WHERE uid = ?''', (-uid,))
+for data in cursor.fetchall():
+    print(hexdump(data[0]))
