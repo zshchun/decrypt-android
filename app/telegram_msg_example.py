91b64263e595bb99bde994542af6327aab285ce0
Seunghwan Chun <zshchun@gmail.com>
2025-08-16 03:10:19 +0900

    show all messages

--- app/telegram_msg_example.py
+++ app/telegram_msg_example.py
@@ -5,11 +5,12 @@ conn = sqlite3.connect('cache4.db')
 cursor = conn.cursor()
 
 # Chatroom UID
-chatroom_title = 'the hacker news'
-cursor.execute('''SELECT uid FROM chats WHERE name = ?''', (chatroom_title,))
-uid = cursor.fetchone()[0]
+#chatroom_title = 'the hacker news'
+#cursor.execute('''SELECT uid FROM chats WHERE name = ?''', (chatroom_title,))
+#uid = cursor.fetchone()[0]
 
 # Show messages
-cursor.execute('''SELECT data FROM messages_v2 WHERE uid = ?''', (-uid,))
+#cursor.execute('''SELECT data FROM messages_v2 WHERE uid = ?''', (-uid,))
+cursor.execute('''SELECT data FROM messages_v2''')
 for data in cursor.fetchall():
     print(hexdump(data[0]))
